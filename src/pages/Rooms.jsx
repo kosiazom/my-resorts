c04c8f814e835f783ee2b2cb33c867b197d62fc6
kosiazom <kosiazom@gmail.com>
2020-11-16 19:03:35 -0500

    adding details

---
+++ src/pages/Rooms.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Rooms = () => {
+    return (
+        <div>
+            Hi I am rooms
+        </div>
+    )
+}
+
+export default Rooms
---
+++ src/pages/SingleRoom.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SingleRoom = () => {
+    return (
+        <div>
+            Hello I am a single rooms page
+        </div>
+    )
+}
+
+export default SingleRoom
