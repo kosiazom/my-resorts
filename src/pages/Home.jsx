865b83775c3545afb9c539a7d91d5991aef45438
kosiazom <kosiazom@gmail.com>
2020-12-02 23:53:50 -0500

    added Banner component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 
+//Parent to Banner ...props are being passed dow
 const Home = () => {
     return (
-        <div>
-           <Hero/>
-           {/* defaultHero is found in the app.css */}
-        </div>
+        
+           <Hero>
+               <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+                   <Link to="/rooms" className="btn-primary">
+                       our rooms
+                   </Link>
+               </Banner>
+               {/* defaultHero is found in the app.css */}
+           </Hero>
+           
+        
     )
 }
 
