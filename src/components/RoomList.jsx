e4c0dcd54ef93f6b8b52133b95af97d0eb91f524
kosiazom <kosiazom@gmail.com>
2021-01-07 12:55:22 -0500

    added room list now working on filter

--- src/components/RoomList.jsx
+++ src/components/RoomList.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import Room from './Room'
 
-const RoomList = () => {
+const RoomList = ({rooms}) => {
+    // console.log(rooms)
+    if(rooms.length === 0) {
+        return(
+            <div className="empty-search">
+                <h3>unfortunately, that room is not available</h3>
+            </div>
+        )
+    }
     return (
-        <div>
-            hello from Room List
-        </div>
+        <section className="roomslist">
+           <div className="roomslist-center">
+               {
+                   rooms.map(item => {
+                       return <Room key={item.id} room={item} />
+                   })
+               }
+           </div>
+        </section>
     );
 }
 
--- src/components/RoomsContainer.jsx
+++ src/components/RoomsContainer.jsx
@@ -1,34 +1,48 @@
 import React from 'react';
 import RoomFilter from './RoomFilter';
 import RoomList from './RoomList'
-import { RoomConsumer } from '../Context'
+import { withRoomConsumer } from '../Context'
 import Loading from './Loading'
 
-const RoomsContainer = () => {
-    return (
-    <RoomConsumer>
-        {value => {
-            const {loading, sortedRooms, rooms} = value;
-            if (loading) {
-                return <Loading />
-            }
-            
-            console.log(value)
-
-            return (
-                <div>
-                   Hello Rooms Container 
-                   <RoomFilter rooms={rooms} />
-                   <RoomList rooms={sortedRooms} />
-                </div>
-            );
-        }}
-
-
-
-
-    </RoomConsumer>
-)
+function RoomContainer({context}) {
+    const {loading, sortedRooms, rooms} = context;
+    if (loading) {
+        return <Loading />
+      }
+      return (
+        <div>
+          <RoomFilter rooms={rooms} />
+          <RoomList rooms={sortedRooms} />
+     </div>
+                
+ )
 }
+export default withRoomConsumer(RoomContainer)
+// const RoomsContainer = () => {
+//     return (
+//     <RoomConsumer>
+//         {value => {
+//             const {loading, sortedRooms, rooms} = value;
+//             if (loading) {
+//                 return <Loading />
+//             }
+            
+//             console.log(value)
 
-export default RoomsContainer;
+//             return (
+//                 <div>
+//                    Hello Rooms Container 
+//                    <RoomFilter rooms={rooms} />
+//                    <RoomList rooms={sortedRooms} />
+//                 </div>
+//             );
+//         }}
+
+
+
+
+//     </RoomConsumer>
+// )
+// }
+
+// export default RoomsContainer;
